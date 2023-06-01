b266d648fff600299c553fcccbebae104a53c66c
Arthur <arthurkglv44@gmail.com>
2023-06-01 06:26:39 -0300

    Foi adicionado try-catch no codigo agora ele não crasha quando usamos letras ou numeros invalidos

--- intro/src/br/java/intro/calculadora/A/Calculadora.java
+++ intro/src/br/java/intro/calculadora/A/Calculadora.java
@@ -1,22 +1,23 @@
 package br.java.intro.calculadora.A;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculadora {
 
-	private static String[] operacoes = { "Soma", "SubtraÁ„o", "Divis„o", "MultiplicaÁ„o", "Raiz Quadrada",
-			"ExponenciaÁo" };
+	private static String[] operacoes = { "Soma", "Subtra√ß√£o", "Divis√£o", "Multiplica√ß√£o", "Raiz Quadrada",
+			"Exponencia√ß√£o" };
 	private static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		listarOperacoes();
 		escolhaUsuario();
 		scan.close();
-		System.out.println("Obrigado trouxa agora trate de usar novamente a calculadora...otario");
+		System.out.println("Obrigado por usar a calculadora braba");
 	}
 
 	private static void listarOperacoes() {
-		System.out.println("Escolha a OperaÁ„o desejada: ");
+		System.out.println("Escolha a Opera√ß√£o desejada: ");
 
 		for (int i = 0; i < operacoes.length; i++) {
 			if (i % 2 != 0) {
@@ -28,83 +29,81 @@ public class Calculadora {
 	}
 
 	private static void escolhaUsuario() {
-
 		int escolha = 999;
 
 		while (escolha != 0) {
-			System.out.println("Insira o n˙mero da operaÁo: (ou digite para desligar a calculadora) ");
-			escolha = scan.nextInt();
-
-			// switch È o equivalente a um interruptor de v·rias posiÁıes
-			switch (escolha) {
-			case 0: {
-				escolha = 0;
-				break;
-			}
-			case 1: {
-				soma();
-				break;
-			}
-			case 2: {
-				subtracao();
-				break;
-			}
-			case 3: {
-				divisao();
-				break;
-			}
-			case 4: {
-				multiplicacao();
-				break;
-			}
-			case 5: {
-				raiz();
-				break;
-			}
-			case 6: {
-				exponenciacao();
-				break;
-			}
-
-			default:
-				throw new IllegalArgumentException("Valor Inesperado:" + escolha);
+			System.out.println("Insira o n√∫mero da opera√ß√£o (ou digite 0 para desligar a calculadora): ");
+			try {
+				escolha = scan.nextInt();
+				switch (escolha) {
+				case 0: {
+					escolha = 0;
+					break;
+				}
+				case 1: {
+					soma();
+					break;
+				}
+				case 2: {
+					subtracao();
+					break;
+				}
+				case 3: {
+					divisao();
+					break;
+				}
+				case 4: {
+					multiplicacao();
+					break;
+				}
+				case 5: {
+					raiz();
+					break;
+				}
+				case 6: {
+					exponenciacao();
+					break;
+				}
+				default:
+					System.out.println("Op√ß√£o inv√°lida. Tente novamente.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Valeu engra√ßad√£o das galaxias, Faz direitinho por favor");
+				scan.next();
 			}
 		}
-
 	}
 
 	private static void soma() {
-		System.out.println("OperaÁ„o de Soma:");
+		System.out.println("Opera√ß√£o de Soma:");
 
-		System.out.println("Primeiro n˙mero:");
+		System.out.println("Primeiro n√∫mero:");
 		int A = scan.nextInt();
-		System.out.println("Segundo n˙mero:");
+		System.out.println("Segundo n√∫mero:");
 		int B = scan.nextInt();
 
 		int resultado = A + B;
 		System.out.println(String.format("%s + %s = %s", A, B, resultado));
-
 	}
 
 	private static void subtracao() {
-		System.out.println("operaÁo de SubtraÁ„o:");
+		System.out.println("Opera√ß√£o de Subtra√ß√£o:");
 
-		System.out.println("Primeiro n˙mero:");
+		System.out.println("Primeiro n√∫mero:");
 		int A = scan.nextInt();
-		System.out.println("Segundo n˙mero:");
+		System.out.println("Segundo n√∫mero:");
 		int B = scan.nextInt();
 
 		int resultado = A - B;
 		System.out.println(String.format("%s - %s = %s", A, B, resultado));
-
 	}
 
 	private static void divisao() {
-		System.out.println("OperaÁ„o de Divis„o");
+		System.out.println("Opera√ß√£o de Divis√£o");
 
-		System.out.println("Primeiro n˙mero:");
+		System.out.println("Primeiro n√∫mero:");
 		int A = scan.nextInt();
-		System.out.println("Segundo n˙mero:");
+		System.out.println("Segundo n√∫mero:");
 		int B = scan.nextInt();
 
 		int resultado = A / B;
@@ -112,11 +111,11 @@ public class Calculadora {
 	}
 
 	private static void multiplicacao() {
-		System.out.println("OperaÁ„o de Divis√£o");
+		System.out.println("Opera√ß√£o de Multiplica√ß√£o");
 
-		System.out.println("Primeiro n˙mero:");
+		System.out.println("Primeiro n√∫mero:");
 		int A = scan.nextInt();
-		System.out.println("Segundo n˙mero:");
+		System.out.println("Segundo n√∫mero:");
 		int B = scan.nextInt();
 
 		int resultado = A * B;
@@ -124,24 +123,34 @@ public class Calculadora {
 	}
 
 	private static void raiz() {
-		System.out.println("OperaÁ„o de Raiz quadrada");
+		System.out.println("Opera√ß√£o de Raiz Quadrada");
 
-		System.out.println("Insira n˙mero:");
-		double A = scan.nextInt();
+		System.out.println("Insira um n√∫mero:");
+		try {
+			double A = scan.nextDouble();
 
-		double resultado = Math.sqrt(A);
-		System.out.println(String.format("A raiz de %s È %s", A, resultado));
+			double resultado = Math.sqrt(A);
+			System.out.println(String.format("A raiz quadrada de %s √© %s", A, resultado));
+		} catch (InputMismatchException e) {
+			System.out.println("Valor inv√°lido. Tente novamente.");
+			scan.next();
+		}
 	}
 
 	private static void exponenciacao() {
-		System.out.println("OperaÁ„o de exponenciaÁ„o");
+		System.out.println("Opera√ß√£o de Exponencia√ß√£o");
 
-		System.out.println("Primeiro n˙mero:");
-		double A = scan.nextInt();
-		System.out.println("Expoente:");
-		double B = scan.nextInt();
+		System.out.println("Base:");
+		try {
+			double A = scan.nextDouble();
+			System.out.println("Expoente:");
+			double B = scan.nextDouble();
 
-		double resultado = Math.pow(A, B);		
-		System.out.println(String.format("%s ^ %s = %s", A, B, resultado));
+			double resultado = Math.pow(A, B);
+			System.out.println(String.format("%s ^ %s = %s", A, B, resultado));
+		} catch (InputMismatchException e) {
+			System.out.println("Valor inv√°lido. Tente novamente.");
+			scan.next();
+		}
 	}
 }
